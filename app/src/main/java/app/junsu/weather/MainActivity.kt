c7c812b0ab7b8f4c4d95022974d6a3f14c41cad9
junsuPark <krpjs0508@gmail.com>
2023-09-06 08:19:09 +0900

    init

--- app/src/main/java/app/junsu/weather/MainActivity.kt
+++ app/src/main/java/app/junsu/weather/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import app.junsu.weather.ui.theme.WeatherBriefingTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +14,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherBriefingTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                    color = MaterialTheme.colorScheme.background,
+                ) {}
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherBriefingTheme {
-        Greeting("Android")
     }
 }
