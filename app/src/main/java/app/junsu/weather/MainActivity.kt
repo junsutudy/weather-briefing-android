e66a4547321535c05fdda23f8c807b3284d414ac
junsuPark <krpjs0508@gmail.com>
2023-09-06 19:29:01 +0900

    card

--- app/src/main/java/app/junsu/weather/MainActivity.kt
+++ app/src/main/java/app/junsu/weather/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,8 +13,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -45,27 +48,56 @@ class MainActivity : ComponentActivity() {
 private fun WeatherApp(
     modifier: Modifier = Modifier,
 ) {
-    val composition by rememberLottieComposition(
-        spec = LottieCompositionSpec.RawRes(R.raw.animation_weather_sunny),
-    )
-
     Column(
         modifier = modifier.fillMaxSize(),
     ) {
         Spacer(modifier = Modifier.weight(0.1f))
-        LottieAnimation(
+        WeatherCard(
             modifier = Modifier
+                .fillMaxWidth()
                 .height(80.dp),
-            composition = composition,
-            iterations = LottieConstants.IterateForever,
-            contentScale = ContentScale.Crop,
         )
         Card(
             modifier = Modifier
                 .weight(0.9f)
                 .fillMaxWidth()
-                .padding(16.dp),
+                .padding(8.dp),
         ) {
+        }
+    }
+}
+
+@Composable
+private fun WeatherCard(
+    modifier: Modifier = Modifier,
+) {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.animation_weather_sunny),
+    )
+
+    Row(
+        modifier = modifier.fillMaxSize(),
+    ) {
+        LottieAnimation(
+            modifier = Modifier.weight(0.4f),
+            composition = composition,
+            iterations = LottieConstants.IterateForever,
+            contentScale = ContentScale.Crop,
+        )
+        Column(
+            modifier = Modifier.weight(0.6f),
+            horizontalAlignment = Alignment.End,
+        ) {
+            Text(
+                modifier = Modifier.padding(end = 16.dp),
+                text = "9월 6일",
+                style = MaterialTheme.typography.bodyMedium,
+            )
+            Text(
+                modifier = Modifier.padding(end = 16.dp),
+                text = "23\'",
+                style = MaterialTheme.typography.displayLarge,
+            )
         }
     }
 }
