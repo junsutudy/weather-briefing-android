9431ac89f6e5a9ef840724a8c3ffcab272d22a7a
junsuPark <krpjs0508@gmail.com>
2023-09-06 17:03:36 +0900

    fun

--- app/src/main/java/app/junsu/weather/data/WeatherStatus.kt
+++ app/src/main/java/app/junsu/weather/data/WeatherStatus.kt
@@ -1,8 +1,16 @@
 package app.junsu.weather.data
 
 enum class WeatherStatus {
-    SUNNY,
-    CLOUDY,
-    RAINY,
-    SNOWY,
+    SUNNY, CLOUDY, RAINY, SNOWY,
+    ;
+
+    companion object {
+        fun fromString(value: String): WeatherStatus = when (value) {
+            "맑음" -> SUNNY
+            "흐림", "구름조금", "구름많음", "번개", "뇌우" -> CLOUDY
+            "비", "약한비", "강한비", "우박", "가끔 비", "비 또는 눈" -> RAINY
+            "눈", "약한눈", "강한눈", "진눈깨비", "소낙눈" -> SNOWY
+            else -> CLOUDY
+        }
+    }
 }
