5b7fe6380bfff57d6e58c406fb519d8975f2851c
junsuPark <krpjs0508@gmail.com>
2023-09-06 16:50:33 +0900

    sample

--- app/src/main/java/app/junsu/weather/MainActivity.kt
+++ app/src/main/java/app/junsu/weather/MainActivity.kt
@@ -3,12 +3,19 @@ package app.junsu.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import app.junsu.weather.ui.theme.WeatherBriefingTheme
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +26,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Card(
+                    WeatherApp(
                         modifier = Modifier.fillMaxSize(),
-                    ) {
-
-                    }
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun WeatherApp(
+    modifier: Modifier = Modifier,
+) {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.animation_lm7elei9),
+    )
+
+    Box(
+        modifier = modifier.fillMaxSize(),
+    ) {
+        LottieAnimation(
+            composition = composition,
+            iterations = LottieConstants.IterateForever,
+            contentScale = ContentScale.Crop,
+        )
     }
 }
