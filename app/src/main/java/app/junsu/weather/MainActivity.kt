f0c5c738e2b9618274fb710e539645638d68ecfa
junsuPark <krpjs0508@gmail.com>
2023-09-06 19:18:51 +0900

    ui

--- app/src/main/java/app/junsu/weather/MainActivity.kt
+++ app/src/main/java/app/junsu/weather/MainActivity.kt
@@ -3,14 +3,20 @@ package app.junsu.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import app.junsu.weather.ui.theme.WeatherBriefingTheme
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
@@ -40,16 +46,26 @@ private fun WeatherApp(
     modifier: Modifier = Modifier,
 ) {
     val composition by rememberLottieComposition(
-        spec = LottieCompositionSpec.RawRes(R.raw.animation_lm7elei9),
+        spec = LottieCompositionSpec.RawRes(R.raw.animation_weather_sunny),
     )
 
-    Box(
+    Column(
         modifier = modifier.fillMaxSize(),
     ) {
+        Spacer(modifier = Modifier.weight(0.1f))
         LottieAnimation(
+            modifier = Modifier
+                .height(80.dp),
             composition = composition,
             iterations = LottieConstants.IterateForever,
             contentScale = ContentScale.Crop,
         )
+        Card(
+            modifier = Modifier
+                .weight(0.9f)
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+        }
     }
 }
