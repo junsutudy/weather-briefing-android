eb95e68a83b585b80bf6029432c889686fc84409
junsuPark <krpjs0508@gmail.com>
2023-09-07 19:43:02 +0900

    dateutil

--- app/src/main/java/app/junsu/weather/util/DateUtils.kt
+++ app/src/main/java/app/junsu/weather/util/DateUtils.kt
@@ -3,19 +3,23 @@ package app.junsu.weather.util
 import java.time.LocalDateTime
 
 enum class TimePart {
-    SUNRISE, MORNING, MIDDAY, AFTERNOON, SUNSET, MIDNIGHT,
+    DAWN, SUNRISE, MORNING, MIDDAY, AFTERNOON, SUNSET, NIGHT, MIDNIGHT,
     ;
 
     companion object {
 
-        fun from(currentTime: LocalDateTime = LocalDateTime.now()) = when (currentTime.hour) {
-            in 2 until 6 -> SUNRISE
-            in 6 until 10 -> MORNING
-            in 10 until 14 -> MIDDAY
-            in 14 until 18 -> AFTERNOON
-            in 18 until 22 -> SUNSET
-            22, 23, 0, 1 -> MIDNIGHT
+        fun from(currentTime: LocalDateTime) = when (currentTime.hour) {
+            in 1 until 5 -> DAWN
+            in 5 until 7 -> SUNRISE
+            in 7 until 11 -> MORNING
+            in 11 until 13 -> MIDDAY
+            in 13 until 17 -> AFTERNOON
+            in 17 until 19 -> SUNSET
+            in 19 until 23 -> NIGHT
+            23, 0 -> MIDNIGHT
             else -> throw IllegalStateException()
         }
+
+        fun fromNow() = this.from(LocalDateTime.now())
     }
 }
