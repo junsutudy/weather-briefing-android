95abf7bdbc88077fadd120a8d24ab7aabf55475a
junsuPark <krpjs0508@gmail.com>
2023-09-08 21:17:25 +0900

    data stream

--- app/src/main/java/app/junsu/weather/data/datasource/network/WeatherNetworkDataSource.kt
+++ app/src/main/java/app/junsu/weather/data/datasource/network/WeatherNetworkDataSource.kt
@@ -2,15 +2,17 @@ package app.junsu.weather.data.datasource.network
 
 import app.junsu.weather.data.Weather
 import app.junsu.weather.data.datasource.network.crawler.WeatherCrawler
+import kotlinx.coroutines.flow.Flow
 
 class WeatherNetworkDataSource(
     private val weatherCrawler: WeatherCrawler,
-) {
+) {/*
     fun fetchWeather(): Weather = Weather(
         temperature = weatherCrawler.temperature,
         weatherStatus = weatherCrawler.weatherStatus,
         fineDustStatus = weatherCrawler.fineDustStatus,
         humidity = weatherCrawler.humidity,
         uvStatus = weatherCrawler.uvStatus,
-    )
+    )*/
+    fun fetchTemperature(): Flow<Float> = weatherCrawler.temperature
 }
--- app/src/main/java/app/junsu/weather/data/repository/WeatherRepository.kt
+++ app/src/main/java/app/junsu/weather/data/repository/WeatherRepository.kt
@@ -1,11 +1,13 @@
 package app.junsu.weather.data.repository
 
-import app.junsu.weather.data.Weather
 import app.junsu.weather.data.datasource.network.WeatherNetworkDataSource
+import kotlinx.coroutines.flow.Flow
 
 class WeatherRepository(
     private val weatherNetworkDataSource: WeatherNetworkDataSource,
 ) {
-    fun fetchWeather(): Weather =
-        weatherNetworkDataSource.fetchWeather()
+    /*
+        fun fetchWeather(): Weather =
+            weatherNetworkDataSource.fetchWeather()*/
+    fun fetchTemperature(): Flow<Float> = weatherNetworkDataSource.fetchTemperature()
 }
