4a57f19c82bddc806eaea6d1ca6d69a70cb5469a
junsuPark <krpjs0508@gmail.com>
2023-09-08 21:17:11 +0900

    injection

--- app/src/main/java/app/junsu/weather/WeatherModule.kt
+++ app/src/main/java/app/junsu/weather/WeatherModule.kt
@@ -3,16 +3,16 @@ package app.junsu.weather
 import app.junsu.weather.data.datasource.network.WeatherNetworkDataSource
 import app.junsu.weather.data.datasource.network.crawler.WeatherCrawler
 import app.junsu.weather.data.repository.WeatherRepository
+import app.junsu.weather.ui.weather.WeatherViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val weatherModule: Module
     get() = module {
         includes(dataModule)
-        viewModelOf(::MainActivityViewModel)
         viewModel<MainActivityViewModel> { MainActivityViewModel(get()) }
+        viewModel<WeatherViewModel> { WeatherViewModel(get()) }
     }
 
 val dataModule: Module
