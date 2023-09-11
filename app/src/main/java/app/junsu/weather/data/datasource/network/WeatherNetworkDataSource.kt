4ec3fd74e521e756ef0d8d72163ad4f1f2b263d8
junsuPark <krpjs0508@gmail.com>
2023-09-11 20:12:18 +0900

    datasource

--- app/src/main/java/app/junsu/weather/data/datasource/network/WeatherNetworkDataSource.kt
+++ app/src/main/java/app/junsu/weather/data/datasource/network/WeatherNetworkDataSource.kt
@@ -1,18 +1,18 @@
 package app.junsu.weather.data.datasource.network
 
-import app.junsu.weather.data.Weather
+import app.junsu.weather.data.FineDustStatus
+import app.junsu.weather.data.Humidity
+import app.junsu.weather.data.UvStatus
+import app.junsu.weather.data.WeatherStatus
 import app.junsu.weather.data.datasource.network.crawler.WeatherCrawler
 import kotlinx.coroutines.flow.Flow
 
 class WeatherNetworkDataSource(
     private val weatherCrawler: WeatherCrawler,
-) {/*
-    fun fetchWeather(): Weather = Weather(
-        temperature = weatherCrawler.temperature,
-        weatherStatus = weatherCrawler.weatherStatus,
-        fineDustStatus = weatherCrawler.fineDustStatus,
-        humidity = weatherCrawler.humidity,
-        uvStatus = weatherCrawler.uvStatus,
-    )*/
+) {
     fun fetchTemperature(): Flow<Float> = weatherCrawler.temperature
+    fun fetchWeatherStatus(): Flow<WeatherStatus> = weatherCrawler.weatherStatus
+    fun fetchFineDustStatus(): Flow<FineDustStatus> = weatherCrawler.fineDustStatus
+    fun fetchHumidity(): Flow<Humidity> = weatherCrawler.humidity
+    fun fetchUvStatus(): Flow<UvStatus> = weatherCrawler.uvStatus
 }
