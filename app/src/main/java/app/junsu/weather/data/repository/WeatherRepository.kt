8d0b4d56b83f11105c223e1df82a5090795d7e91
junsuPark <krpjs0508@gmail.com>
2023-09-11 20:27:51 +0900

    res

--- app/src/main/java/app/junsu/weather/data/repository/WeatherRepository.kt
+++ app/src/main/java/app/junsu/weather/data/repository/WeatherRepository.kt
@@ -1,13 +1,19 @@
 package app.junsu.weather.data.repository
 
+import app.junsu.weather.data.Weather
 import app.junsu.weather.data.datasource.network.WeatherNetworkDataSource
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 
 class WeatherRepository(
     private val weatherNetworkDataSource: WeatherNetworkDataSource,
 ) {
-    /*
-        fun fetchWeather(): Weather =
-            weatherNetworkDataSource.fetchWeather()*/
-    fun fetchTemperature(): Flow<Float> = weatherNetworkDataSource.fetchTemperature()
+    suspend fun fetchWeather(): Weather {
+        return Weather(
+            temperature = weatherNetworkDataSource.fetchTemperature().first(),
+            weatherStatus = weatherNetworkDataSource.fetchWeatherStatus().first(),
+            fineDustStatus = weatherNetworkDataSource.fetchFineDustStatus().first(),
+            humidity = weatherNetworkDataSource.fetchHumidity().first(),
+            uvStatus = weatherNetworkDataSource.fetchUvStatus().first(),
+        )
+    }
 }
