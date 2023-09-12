e604368e6b0737b4b531893b8b03cf1890d2f7c2
junsuPark <krpjs0508@gmail.com>
2023-09-13 07:30:40 +0900

    news network

---
+++ app/src/main/java/app/junsu/weather/data/datasource/network/NewsNetworkDataSource.kt
@@ -0,0 +1,9 @@
+package app.junsu.weather.data.datasource.network
+
+import app.junsu.weather.data.datasource.network.crawler.NewsCrawler
+
+class NewsNetworkDataSource (
+    private val newsCrawler: NewsCrawler,
+){
+
+}
