3608ad90e2abfb784e92456dca23e48861787029
junsuPark <krpjs0508@gmail.com>
2023-09-13 07:33:27 +0900

    uv status string

--- app/src/main/java/app/junsu/weather/data/UvStatus.kt
+++ app/src/main/java/app/junsu/weather/data/UvStatus.kt
@@ -4,15 +4,15 @@ enum class UvStatus {
     VERY_HIGH,
     HIGH,
     NORMAL,
-    LOW,
+    GOOD,
     ;
 
     companion object {
         fun fromString(value: String): UvStatus = when (value) {
-            "매우", "매우높음" -> VERY_HIGH
-            "높음" -> HIGH
-            "보통" -> NORMAL
-            "좋음" -> LOW
+            "자외선 매우", "자외선 매우높음" -> VERY_HIGH
+            "자외선 높음" -> HIGH
+            "자외선 보통" -> NORMAL
+            "자외선 좋음" -> GOOD
             else -> NORMAL
         }
     }
