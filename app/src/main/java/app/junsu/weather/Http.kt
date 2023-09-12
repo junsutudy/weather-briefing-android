1349ee0f6ac2e4cd3b272cf2506093a62ee84fde
junsuPark <krpjs0508@gmail.com>
2023-09-13 07:29:58 +0900

    di

--- app/src/main/java/app/junsu/weather/Http.kt
+++ app/src/main/java/app/junsu/weather/Http.kt
@@ -2,3 +2,6 @@ package app.junsu.weather
 
 const val searchedWeatherUrl: String =
     "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%82%A0%EC%94%A8&oquery=%EB%8C%80%EC%A0%84+%EB%82%A0%EC%94%A8&tqi=inQlhlp0J1Zss4KMcMRssssssZG-256271"
+
+const val searchedNewsUrl: String =
+    "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%89%B4%EC%8A%A4&oquery=%EB%82%A0%EC%94%A8&tqi=idtIKwp0JywssuCrUEsssssstV0-072100"
