5c77d6e1c32f4ee2ff3cee95a25b40146f65d3b8
junsuPark <krpjs0508@gmail.com>
2023-09-13 07:30:12 +0900

    di

--- app/src/main/java/app/junsu/weather/WeatherModule.kt
+++ app/src/main/java/app/junsu/weather/WeatherModule.kt
@@ -1,7 +1,10 @@
 package app.junsu.weather
 
+import app.junsu.weather.data.datasource.network.NewsNetworkDataSource
 import app.junsu.weather.data.datasource.network.WeatherNetworkDataSource
+import app.junsu.weather.data.datasource.network.crawler.NewsCrawler
 import app.junsu.weather.data.datasource.network.crawler.WeatherCrawler
+import app.junsu.weather.data.repository.NewsRepository
 import app.junsu.weather.data.repository.WeatherRepository
 import app.junsu.weather.ui.weather.WeatherViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -17,7 +20,10 @@ val weatherModule: Module
 
 val dataModule: Module
     get() = module {
+        single<NewsCrawler> { NewsCrawler(searchedNewsUrl) }
         single<WeatherCrawler> { WeatherCrawler(searchedWeatherUrl) }
+        single<NewsNetworkDataSource> { NewsNetworkDataSource(get()) }
         single<WeatherNetworkDataSource> { WeatherNetworkDataSource(get()) }
+        single<NewsRepository> { NewsRepository(get()) }
         single<WeatherRepository> { WeatherRepository(get()) }
     }
