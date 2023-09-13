4e015dc369bc0f491db466f0907621e4a65aca90
junsuPark <krpjs0508@gmail.com>
2023-09-13 19:08:21 +0900

    glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.ktor.client.serialization)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.jsoup)
+    implementation(libs.coil.compose)
     implementation(libs.lottie.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
