ea8478ad6dcb0085a8a7328c9d149d1b4d98c258
junsuPark <krpjs0508@gmail.com>
2023-09-13 13:08:11 +0900

    main vm

--- app/src/main/java/app/junsu/weather/MainActivityViewModel.kt
+++ app/src/main/java/app/junsu/weather/MainActivityViewModel.kt
@@ -1,8 +1,5 @@
 package app.junsu.weather
 
 import androidx.lifecycle.ViewModel
-import app.junsu.weather.data.repository.WeatherRepository
 
-class MainActivityViewModel(
-    private val weatherRepository: WeatherRepository,
-) : ViewModel()
+class MainActivityViewModel : ViewModel()
