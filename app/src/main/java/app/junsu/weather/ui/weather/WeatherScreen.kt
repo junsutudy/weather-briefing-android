aa4b1ef16efd9847ac2f9b99096c5103dc74f7c4
junsuPark <krpjs0508@gmail.com>
2023-09-14 15:09:01 +0900

    card

--- app/src/main/java/app/junsu/weather/ui/weather/WeatherScreen.kt
+++ app/src/main/java/app/junsu/weather/ui/weather/WeatherScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
@@ -346,16 +347,15 @@ private fun HumidityCard(
                 .height(80.dp)
                 .fillMaxWidth(),
         ) {
-            Card(
+            Box(
                 modifier = Modifier
+                    .background(
+                        color = currentBackgroundColor,
+                        shape = RoundedCornerShape(4.dp),
+                    )
                     .width(4.dp)
                     .fillMaxHeight(),
-                colors = CardDefaults.cardColors(
-                    containerColor = currentBackgroundColor,
-                ),
-            ) {
-
-            }
+            )
         }
     }
 }
